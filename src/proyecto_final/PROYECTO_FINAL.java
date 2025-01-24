2e87d4faf6cb7652ac2ad68e7bd7d5ea1edf00e2
ASUS <ASUS@LAPTOP-MMHFPRLH>
2025-01-24 16:44:55 -0500

    carga inicial

---
+++ src/proyecto_final/PROYECTO_FINAL.java
@@ -0,0 +1,33 @@
+
+package proyecto_final;
+
+import java.util.Scanner;
+public class PROYECTO_FINAL {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Declarar variables
+    int vt1, vt2, vt3;
+    double sm, pt, c;
+    String vendedor;
+    Scanner lectura=new Scanner(System.in);
+    //entrada de datos
+    System.out.print("Nombre del vendedor: ");
+    vendedor=lectura.next();
+    System.out.print("Sueldo mensual: ");
+    sm=lectura.nextDouble();
+    System.out.print("Venta 1: ");
+    vt1=lectura.nextInt();
+    System.out.print("Venta 2: ");
+    vt2=lectura.nextInt();
+    System.out.print("Venta 3: ");
+    vt3=lectura.nextInt();
+    //proceso
+    c=(vt1+vt2+vt3)*0.10;
+    pt=sm+c;
+    //salida
+    System.out.println("La comisión de la venta es       :"+c);
+    System.out.println("El sueldo mensual del vendedor es:"+pt);
+    }
+    
+}
